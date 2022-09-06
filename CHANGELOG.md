fe8bc3ef27e514ae897138f984524710d080fcdb
David Hewitt <1939362+davidhewitt@users.noreply.github.com>
2022-09-06 08:13:21 +0100

    match any first line

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,10 @@
 # Change Log
 All notable changes to the "shebang-language-associator" extension will be documented in this file.
 
+## [1.4.0]
+- Extend support to any first-line regex.
+- When matching, associations defined earlier in `shebang.associations` setting will now take priority over later ones (previously later ones would take priority).
+
 ## [1.3.0]
 - Add new option `shebang.associateOnSave` (default `true`) to also check files for language associations on save. Previous versions of the extension only ran on file open.
 
--- src/extension.ts
+++ src/extension.ts
@@ -6,88 +6,92 @@ import * as vscode from 'vscode';
  * Called by vscode on first activation of the plugin.
  */
 export function activate(context: vscode.ExtensionContext) {
-    /*
-     * Any files open on startup need to have shebang checked.
-     */
-    checkAllFiles();
+  /*
+   * Any files open on startup need to have shebang checked.
+   */
+  reconfigureAndAssociate();
 
-    /*
-     * Re-check when configuration changes.
-     */
-    let disposable = vscode.workspace.onDidChangeConfiguration(checkAllFiles);
-    context.subscriptions.push(disposable);
+  /*
+   * Re-check when configuration changes.
+   */
+  let disposable = vscode.workspace.onDidChangeConfiguration(
+    reconfigureAndAssociate
+  );
+  context.subscriptions.push(disposable);
 
-    /*
-     * And also when further files are opened we will check them.
-     */
-    disposable = vscode.workspace.onDidOpenTextDocument(checkFile);
-    context.subscriptions.push(disposable);
-    /*
-     * And also when files are saved.
-     */
-    disposable = vscode.workspace.onDidSaveTextDocument((doc) => {
-      if (
-        vscode.workspace
-          .getConfiguration("shebang")
-          .get<boolean>("associateOnSave")
-      ) {
-        checkFile(doc);
-      }
-    });
-    context.subscriptions.push(disposable);
+  /*
+   * And also when further files are opened we will check them.
+   */
+  disposable = vscode.workspace.onDidOpenTextDocument(associateFile);
+  context.subscriptions.push(disposable);
+  /*
+   * And also when files are saved.
+   */
+  disposable = vscode.workspace.onDidSaveTextDocument((doc) => {
+    if (
+      vscode.workspace
+        .getConfiguration("shebang")
+        .get<boolean>("associateOnSave")
+    ) {
+      associateFile(doc);
+    }
+  });
+  context.subscriptions.push(disposable);
 }
 
 /**
  * Called by vscode when extension is deactivated
  */
-export function deactivate() {
-}
+export function deactivate() {}
 
 /**
- * Re-check all open files
+ * Builds associations global and then associates all files with current settings
  */
-function checkAllFiles() {
-    for (const td of vscode.workspace.textDocuments) {
-        checkFile(td);
-    }
-}
+function reconfigureAndAssociate() {
+  let associationsSettings = vscode.workspace
+    .getConfiguration("shebang")
+    .get<Array<AssociationSetting>>("associations");
 
-/**
- * Accepted prefixes are:
- * #! - shebang
- * #compdef - zsh
- * /// - POSIX path
- */
-const acceptedPrefixes: Array<string> = [
-    "#!",
-    "#compdef",
-    "//",
-]
+  if (associationsSettings) {
+    associations = associationsSettings.map((assoc) => ({
+      pattern: new RegExp(assoc.pattern),
+      language: assoc.language,
+    }));
+  } else {
+    associations = [];
+  }
+
+  for (const td of vscode.workspace.textDocuments) {
+    associateFile(td);
+  }
+}
 
 /**
  * Check whether a file has a matching shebang, and apply the appropriate
  * language mode if so.
  */
-function checkFile(doc: vscode.TextDocument) {
-    let shebang = doc.lineAt(0);
+function associateFile(doc: vscode.TextDocument) {
+  let shebang = doc.lineAt(0);
 
-    /*
-     * Do nothing if the first line is not a shebang-like line.
-     */
-    const match = acceptedPrefixes.some(prefix => shebang.text.startsWith(prefix));
-    if (!match) {
-        return;
+  for (const association of associations) {
+    if (shebang.text.match(association.pattern)) {
+      vscode.languages.setTextDocumentLanguage(doc, association.language);
+      return;
     }
-
-    let associations =
-        vscode.workspace.getConfiguration("shebang")
-            .get<Array<any>>("associations");
-
-    if (associations) {
-        for (const association of associations) {
-            if (shebang.text.match(new RegExp(association.pattern))) {
-                vscode.languages.setTextDocumentLanguage(doc, association.language);
-            }
-        }
-    }
+  }
 }
+
+type AssociationSetting = {
+  pattern: string;
+  language: string;
+};
+
+type Association = {
+  pattern: RegExp;
+  language: string;
+};
+
+/**
+ * Global state of the plugin.
+ */
+let associations: Array<Association> = [];
